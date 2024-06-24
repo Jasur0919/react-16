3cf0c61d94a11a43d2a5098325109dc60877f906
Jasur0919 <jasurjohn1111@gmail.com>
2024-06-24 14:53:46 +0500

    modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+
 import './App.css'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="h1">
-        <h1>salom</h1>
-      </div>
+      <Outlet/>
     </>
   )
 }
